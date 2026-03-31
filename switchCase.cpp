36b233e8f0188751c30809cddd7e9f0ba9004318
Rizhuycihuy <rizki003131@gmail.com>
2026-03-31 20:07:02 +0700

    void output

--- switchCase.cpp
+++ switchCase.cpp
@@ -36,3 +36,12 @@ void pilihanJurusan(){
       }
 }
 
+void output(){
+  cout << "\n=== HASIL SELEKSI ===" << endl;
+  cout << "Jurusan Pilihan"<< namaJurusan << endl;
+  cout << "Status Kuota : " << statusKuota << endl;
+  cout << "-------------------------------" <<endl;
+}
+
+
+
