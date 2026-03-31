9178c21f2313f586c2265bd8b5cf2f869f058c26
Rizhuycihuy <rizki003131@gmail.com>
2026-03-31 19:32:50 +0700

    looping final

---
+++ looping.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(){
+  int x;
+    cout <<"perulangan dengan for"<<endl;
+  for(int i; i<=4; i++){
+      cout <<"Teknologi informasi umy"<<endl;
+  }
+  cout<<endl;
+  srand(time(0));
+
+  cout <<"perulangan while"<<endl;
+  x = 1 + rand()%10;
+
+  while(x<=5){
+        cout<<"bilangan acak = "<<x <<endl;
+        x = rand()%10;
+  }
+
+  cout<<"bilangan acak while yang terakhir ="<<x <<endl;
+  cout<<endl;  
+}
