c0c0cb7e290b94b91d7c7fa518dbfad0b550b821
Rizhuycihuy <rizki003131@gmail.com>
2026-03-31 18:57:52 +0700

    perulangan dengan for

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <ctime>
+using namespace std;
 
+int main(){
+  int x;
+    cout <<"perulangan dengan for"<<endl;
+  for(int i; i<=4; i++){
+      cout <<"Teknologi informasi umy"<<endl;
+  }
+  cout <<endl;
+}
