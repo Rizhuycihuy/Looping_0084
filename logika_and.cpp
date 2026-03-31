032ae2495a71120155613b923d3da57b66445a93
Rizhuycihuy <rizki003131@gmail.com>
2026-03-31 19:30:21 +0700

    logika and

---
+++ logika_and.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  float nilbi,nilm,rerata;
+  string status;
+
+  cout<<"masukkan nilai mtk = ";
+  cin>>nilm;
+  cout<<"masukkan nilai bahasa ingris = ";
+  cin>> nilbi;
+
+  rerata = (nilbi+nilm)/2;
+
+  if (!(rerata >= 60) && (nilm >= 70))
+      status = "lulus";
+  else
+      status = "tidak lulus";
+  
+  cout<<"status kelulusan = "<<status<<" ,dengan nilai rata rata = "<<rerata<<endl;
+}
